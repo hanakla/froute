9140b7e17b8c70ab78dd0cbb777aa82b1f9d1085
Hanakla <8597982+hanakla@users.noreply.github.com>
2020-12-21 20:22:12 +0900

    Expose FrouteLink

---
+++ src/components/FrouteLink.tsx
@@ -0,0 +1,41 @@
+import React, { forwardRef, useMemo, Ref, useRef, ReactElement } from "react";
+import { useUrlBuilder } from "../react-bind";
+import { Link } from "./Link";
+import { ParamsOfRoute, RouteDefinition, routeOf } from "../RouteDefiner";
+
+type NativeProps = Omit<
+  React.DetailedHTMLProps<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  >,
+  "href"
+>;
+
+type OwnProps<R extends RouteDefinition<any>> = {
+  ref?: Ref<HTMLAnchorElement>;
+  to: R;
+  params: ParamsOfRoute<R>;
+  query?: { [key: string]: string | string[] };
+};
+
+type Props<R extends RouteDefinition<any>> =
+  // NativeProps &
+  OwnProps<R>;
+
+type FrouteLink = <R extends RouteDefinition<any>>(
+  props: Props<R>
+) => ReactElement;
+
+export const FrouteLink: FrouteLink = forwardRef(
+  ({ to, params, query, ...props }, ref) => {
+    const { buildPath } = useUrlBuilder();
+
+    const href = useMemo(() => buildPath(to, params, query), [
+      buildPath,
+      params,
+      query,
+    ]);
+
+    return <Link ref={ref} {...props} href={href} />;
+  }
+);
--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export {
   FrouteContext,
 } from "./react-bind";
 export { Link } from "./components/Link";
+export { FrouteLink } from "./components/FrouteLink";
 export { ResponseCode } from "./components/ResponseCode";
 export { Redirect } from "./components/Redirect";
 export { matchByRoutes, isMatchToRoute, FrouteMatch } from "./routing";
