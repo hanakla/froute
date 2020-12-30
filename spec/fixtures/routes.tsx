f32b429fff2af4da9e2a106dadb2b6e32364c420
Hanakla <8597982+hanakla@users.noreply.github.com>
2020-12-31 03:03:33 +0900

    Replace routeBy to routeOf

--- spec/fixtures/routes.tsx
+++ spec/fixtures/routes.tsx
@@ -1,34 +1,28 @@
 import { useParams } from "../../src/react-bind";
-import { routeBy } from "../../src/RouteDefiner";
+import { routeOf } from "../../src/RouteDefiner";
 
 export const complexRoutes = {
-  usersShow: routeBy("/users")
-    .param("id")
-    .action({
-      component: () => {
-        const Component = () => {
-          const params = useParams(complexRoutes.usersShow);
-          return <div>I am user {params.id}</div>;
-        };
-        return new Promise((resolve) =>
-          setTimeout(() => resolve(Component), 100)
+  usersShow: routeOf("/users/:id").action({
+    component: () => {
+      const Component = () => {
+        const params = useParams(complexRoutes.usersShow);
+        return <div>I am user {params.id}</div>;
+      };
+      return new Promise((resolve) =>
+        setTimeout(() => resolve(Component), 100)
+      );
+    },
+  }),
+  userArtworks: routeOf("/users/:id/artworks/:artworkId").action({
+    component: () => {
+      return () => {
+        const params = useParams();
+        return (
+          <div>
+            Here is Artwork {params.artworkId} for user {params.id}
+          </div>
         );
-      },
-    }),
-  userArtworks: routeBy("/users")
-    .param("id")
-    .path("artworks")
-    .param("artworkId")
-    .action({
-      component: () => {
-        return () => {
-          const params = useParams();
-          return (
-            <div>
-              Here is Artwork {params.artworkId} for user {params.id}
-            </div>
-          );
-        };
-      },
-    }),
+      };
+    },
+  }),
 };
