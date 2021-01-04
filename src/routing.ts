f53e7bdabd2abb323b54633ab4522b297632f15a
Hanakla <8597982+hanakla@users.noreply.github.com>
2021-01-04 15:51:32 +0900

    Prevent infinite loop by RouterContext.resolveRoute

--- src/routing.ts
+++ src/routing.ts
@@ -28,11 +28,51 @@ export interface RouteResolver {
   ): FrouteMatch<any> | null;
 }
 
+type RoutesObject = {
+  [key: string]: RouteDefinition<any, any>;
+};
+
+const createRoutingOnlyContext = (
+  context: RouterContext | RoutingOnlyRouterContext | undefined,
+  routes: RoutesObject
+): RoutingOnlyRouterContext => {
+  if (context) {
+    const ctx: RoutingOnlyRouterContext = {
+      get statusCode() {
+        return context!.statusCode;
+      },
+      set statusCode(status: number) {
+        context!.statusCode = status;
+      },
+      get redirectTo() {
+        return context!.redirectTo;
+      },
+      set redirectTo(url: string | null) {
+        context!.redirectTo = url;
+      },
+      resolveRoute: (pathname) =>
+        matchByRoutes(pathname, routes, { context: ctx }),
+    };
+
+    return ctx;
+  }
+
+  const stabCtx: RoutingOnlyRouterContext = {
+    statusCode: 200,
+    redirectTo: null,
+    resolveRoute: (pathname) =>
+      matchByRoutes(pathname, routes, {
+        /* skip resolver: for guard from infinite loop */
+        context: stabCtx,
+      }),
+  };
+
+  return stabCtx;
+};
+
 export const matchByRoutes = (
   pathname: string,
-  routes: {
-    [key: string]: RouteDefinition<any, any>;
-  },
+  routes: RoutesObject,
   {
     resolver,
     context,
@@ -41,15 +81,7 @@ export const matchByRoutes = (
     context?: RoutingOnlyRouterContext;
   } = {}
 ): FrouteMatch<any> | null => {
-  const usingContext: RoutingOnlyRouterContext = context ?? {
-    redirectTo: null as string | null,
-    statusCode: 200,
-    resolveRoute: (pathname) =>
-      matchByRoutes(pathname, routes, {
-        /* skip resolver: for guard from infinite loop */
-        context: usingContext,
-      }),
-  };
+  const usingContext = createRoutingOnlyContext(context, routes);
 
   const parsed = parseUrl(pathname);
 
