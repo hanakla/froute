e6d78f2816c6a1f89fcda9ef2ca5a7f7afd62c7f
Hanakla / ragg <ragg@pixiv.com>
2021-01-18 01:17:44 +0900

    spec: Refactor, move deprecated hooks into one describe

--- src/react-bind.spec.tsx
+++ src/react-bind.spec.tsx
@@ -184,178 +184,6 @@ describe("react-bind", () => {
     });
   });
 
-  describe("useLocation", () => {
-    it("Should correctry parsed complex url", async () => {
-      const router = createRouter(routes);
-      await router.navigate("/users/1?q=1#hash");
-
-      const result = renderHook(() => useLocation(), {
-        wrapper: createWrapper(router),
-      });
-
-      expect(result.result.current).toMatchInlineSnapshot(`
-        Object {
-          "hash": "#hash",
-          "key": "",
-          "pathname": "/users/1",
-          "query": Object {
-            "q": "1",
-          },
-          "search": "?q=1",
-          "state": Object {
-            "hist": 1,
-          },
-        }
-      `);
-    });
-
-    it("in 404, returns location and empty query", async () => {
-      const router = createRouter(routes);
-      await router.navigate("/notfound");
-
-      const result = renderHook(() => useLocation(), {
-        wrapper: createWrapper(router),
-      });
-
-      expect(result.result.current).toMatchInlineSnapshot(`
-        Object {
-          "hash": "",
-          "key": "",
-          "pathname": "/notfound",
-          "query": Object {},
-          "search": "",
-          "state": null,
-        }
-      `);
-    });
-  });
-
-  describe("useHistoryState", () => {
-    it("get / set", async () => {
-      const router = createRouter(routes);
-      await router.navigate("/users");
-
-      const {
-        result: {
-          current: [get, set],
-        },
-        rerender,
-      } = renderHook(() => useHistoryState(routes.usersShow), {
-        wrapper: createWrapper(router),
-      });
-
-      expect(get()).toMatchInlineSnapshot(`null`);
-
-      set({ hist: 1 });
-      rerender();
-
-      expect(get()).toMatchInlineSnapshot(`
-        Object {
-          "hist": 1,
-        }
-      `);
-    });
-
-    it("Logging if expected route isnt match", async () => {
-      jest.mock("./utils", () => ({
-        isDevelopment: true,
-        canUseDom: () => true,
-      }));
-
-      const router = createRouter(routes);
-      await router.navigate("/users");
-
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      const spy = jest.spyOn(console, "warn").mockImplementation(() => {});
-      renderHook(() => useHistoryState(routes.userArtworks), {
-        wrapper: createWrapper(router),
-      });
-
-      expect(spy.mock.calls.length).toBe(1);
-    });
-  });
-
-  describe("useParams", () => {
-    it("test", async () => {
-      const router = createRouter(routes);
-
-      await router.navigate("/users/1");
-      const result = renderHook(
-        () => {
-          return useParams(routes.usersShow);
-        },
-        {
-          wrapper: createWrapper(router),
-        }
-      );
-
-      expect(result.result.current).toMatchInlineSnapshot(`
-        Object {
-          "id": "1",
-        }
-      `);
-
-      await router.navigate("/users/1/artworks/1");
-      result.rerender();
-
-      expect(result.result.current).toMatchInlineSnapshot(`
-        Object {
-          "artworkId": "1",
-          "id": "1",
-        }
-      `);
-    });
-  });
-
-  describe("useNavigation", () => {
-    it("", async () => {
-      const router = createRouter(routes);
-      await router.navigate("/users/1");
-
-      const { result } = renderHook(useNavigation, {
-        wrapper: createWrapper(router),
-      });
-
-      result.current.push(routes.usersShow, { id: "2" });
-      await waitTick(200);
-
-      expect(router.getCurrentLocation()).toMatchObject({
-        hash: "",
-        pathname: "/users/2",
-        search: "",
-        state: {
-          __froute: {
-            scrollX: 0,
-            scrollY: 0,
-          },
-          app: { hist: 1 },
-        },
-      });
-
-      expect(location.href).toMatchInlineSnapshot(`"http://localhost/users/2"`);
-
-      result.current.push(routes.userArtworks, { id: "1", artworkId: "2" });
-      await waitTick(200);
-
-      expect(router.getCurrentLocation()).toMatchObject({
-        hash: "",
-        pathname: "/users/1/artworks/2",
-        search: "",
-        state: {
-          __froute: {
-            scrollX: 0,
-            scrollY: 0,
-          },
-          app: null,
-        },
-      });
-
-      expect(location.href).toMatchInlineSnapshot(
-        `"http://localhost/users/1/artworks/2"`
-      );
-    });
-  });
-
   describe("useRouteComponent", () => {
     it("test", async () => {
       const router = createRouter(routes);
@@ -388,15 +216,191 @@ describe("react-bind", () => {
     });
   });
 
-  describe("useUrlBuilder", () => {
-    const router = createRouter(routes);
+  describe("deprecated hooks", () => {
+    describe("useLocation", () => {
+      it("Should correctry parsed complex url", async () => {
+        const router = createRouter(routes);
+        await router.navigate("/users/1?q=1#hash");
 
-    const { result } = renderHook(useUrlBuilder, {
-      wrapper: createWrapper(router),
+        const result = renderHook(() => useLocation(), {
+          wrapper: createWrapper(router),
+        });
+
+        expect(result.result.current).toMatchInlineSnapshot(`
+        Object {
+          "hash": "#hash",
+          "key": "",
+          "pathname": "/users/1",
+          "query": Object {
+            "q": "1",
+          },
+          "search": "?q=1",
+          "state": Object {
+            "hist": 1,
+          },
+        }
+      `);
+      });
+
+      it("in 404, returns location and empty query", async () => {
+        const router = createRouter(routes);
+        await router.navigate("/notfound");
+
+        const result = renderHook(() => useLocation(), {
+          wrapper: createWrapper(router),
+        });
+
+        expect(result.result.current).toMatchInlineSnapshot(`
+        Object {
+          "hash": "",
+          "key": "",
+          "pathname": "/notfound",
+          "query": Object {},
+          "search": "",
+          "state": null,
+        }
+      `);
+      });
     });
 
-    expect(
-      result.current.buildPath(routes.usersShow, { id: "1" })
-    ).toMatchInlineSnapshot(`"/users/1"`);
+    describe("useHistoryState", () => {
+      it("get / set", async () => {
+        const router = createRouter(routes);
+        await router.navigate("/users");
+
+        const {
+          result: {
+            current: [get, set],
+          },
+          rerender,
+        } = renderHook(() => useHistoryState(routes.usersShow), {
+          wrapper: createWrapper(router),
+        });
+
+        expect(get()).toMatchInlineSnapshot(`null`);
+
+        set({ hist: 1 });
+        rerender();
+
+        expect(get()).toMatchInlineSnapshot(`
+        Object {
+          "hist": 1,
+        }
+      `);
+      });
+
+      it("Logging if expected route isnt match", async () => {
+        jest.mock("./utils", () => ({
+          isDevelopment: true,
+          canUseDom: () => true,
+        }));
+
+        const router = createRouter(routes);
+        await router.navigate("/users");
+
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const spy = jest.spyOn(console, "warn").mockImplementation(() => {});
+        renderHook(() => useHistoryState(routes.userArtworks), {
+          wrapper: createWrapper(router),
+        });
+
+        expect(spy.mock.calls.length).toBe(1);
+      });
+    });
+
+    describe("useParams", () => {
+      it("test", async () => {
+        const router = createRouter(routes);
+
+        await router.navigate("/users/1");
+        const result = renderHook(
+          () => {
+            return useParams(routes.usersShow);
+          },
+          {
+            wrapper: createWrapper(router),
+          }
+        );
+
+        expect(result.result.current).toMatchInlineSnapshot(`
+        Object {
+          "id": "1",
+        }
+      `);
+
+        await router.navigate("/users/1/artworks/1");
+        result.rerender();
+
+        expect(result.result.current).toMatchInlineSnapshot(`
+        Object {
+          "artworkId": "1",
+          "id": "1",
+        }
+      `);
+      });
+    });
+
+    describe("useNavigation", () => {
+      it("", async () => {
+        const router = createRouter(routes);
+        await router.navigate("/users/1");
+
+        const { result } = renderHook(useNavigation, {
+          wrapper: createWrapper(router),
+        });
+
+        result.current.push(routes.usersShow, { id: "2" });
+        await waitTick(200);
+
+        expect(router.getCurrentLocation()).toMatchObject({
+          hash: "",
+          pathname: "/users/2",
+          search: "",
+          state: {
+            __froute: {
+              scrollX: 0,
+              scrollY: 0,
+            },
+            app: { hist: 1 },
+          },
+        });
+
+        expect(location.href).toMatchInlineSnapshot(
+          `"http://localhost/users/2"`
+        );
+
+        result.current.push(routes.userArtworks, { id: "1", artworkId: "2" });
+        await waitTick(200);
+
+        expect(router.getCurrentLocation()).toMatchObject({
+          hash: "",
+          pathname: "/users/1/artworks/2",
+          search: "",
+          state: {
+            __froute: {
+              scrollX: 0,
+              scrollY: 0,
+            },
+            app: null,
+          },
+        });
+
+        expect(location.href).toMatchInlineSnapshot(
+          `"http://localhost/users/1/artworks/2"`
+        );
+      });
+    });
+
+    describe("useUrlBuilder", () => {
+      const router = createRouter(routes);
+
+      const { result } = renderHook(useUrlBuilder, {
+        wrapper: createWrapper(router),
+      });
+
+      expect(
+        result.current.buildPath(routes.usersShow, { id: "1" })
+      ).toMatchInlineSnapshot(`"/users/1"`);
+    });
   });
 });
