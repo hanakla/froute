b90e140d0a24690af5dfcb1e40e039b4c3718adf
Hanakla / ragg <ragg@pixiv.com>
2021-02-10 18:42:56 +0900

    Update example

--- packages/examples/src/routes.ts
+++ packages/examples/src/routes.ts
@@ -5,13 +5,18 @@ import { UserOps } from "./domains";
 export const routes = {
   index: routeOf("/").action({
     component: () => import("./pages/index"),
+    preload: () => new Promise<void>((r) => setTimeout(r, 1000)),
   }),
   userShow: routeOf("/users/:id").action({
     component: () => import("./pages/user"),
     preload: (context: OperationContext, { id }) =>
-      Promise.all([context.executeOperation(UserOps.fetchUser, id)]),
+      Promise.all([
+        new Promise<void>((r) => setTimeout(r, 1000)),
+        context.executeOperation(UserOps.fetchUser, id),
+      ]),
   }),
   beforeUnload: routeOf("/beforeunload").action({
     component: () => import("./pages/beforeunload"),
+    preload: () => new Promise<void>((r) => setTimeout(r, 1000)),
   }),
 };
