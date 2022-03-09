a8bd1ad5acffafd5549a207503d627b37702eba1
Hanakla / ragg <ragg@pixiv.com>
2022-03-09 14:00:19 +0900

    target attribute is no longer ignored on Link component

--- packages/froute/src/components/Link.tsx
+++ packages/froute/src/components/Link.tsx
@@ -29,14 +29,15 @@ export const Link = forwardRef<
     (e: React.MouseEvent<HTMLAnchorElement>) => {
       if (onClick) onClick(e);
       if (e.isDefaultPrevented()) return;
+
       if (!href) return;
       if (isModifiedEvent(e)) return;
       if (!isRoutable(href)) return;
+      if (e.currentTarget.target !== "") return;
 
       e.preventDefault();
 
       const parsed = parseUrl(href);
-
       push(
         (parsed.pathname || "") + (parsed.search || "") + (parsed.hash || "")
       );
